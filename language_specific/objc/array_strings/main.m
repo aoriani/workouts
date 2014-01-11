0527b8de870ba75f9cc22c715b37b1e63961e343
André Oriani <aoriani@users.noreply.github.com>
2014-01-11 13:52:41 -0200

    Some formatting

--- language_specific/objc/array_strings/main.m
+++ language_specific/objc/array_strings/main.m
@@ -2,13 +2,11 @@
 
 int main (int argc, char* argv[]){
 	@autoreleasepool{
-		NSArray* list = [NSArray arrayWithObjects: @"A", @"B",
-                         @"C",nil];
+		NSArray* list = [NSArray arrayWithObjects: @"A", @"B", @"C",nil];
         
 		for(NSString* s in list) {
-			NSLog(@"%@",s);
+			NSLog(@"%@", s);
 		}
 	}
 	return 0;
 }
-
